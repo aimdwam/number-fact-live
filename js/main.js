a4819340be0b0b6bd56f5fe91c1e975df51ae709
Chris Cheung <chris.ch.cheung@gmail.com>
2020-03-19 08:35:00 +0800

    handle checkBtn click event

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ console.log("main.js loaded")
 
 function onLoad() {
     console.log("body onload event fired")
+
+    let checkBtn = document.getElementById("checkBtn")
+    let handleClick = evt => {
+        console.log('clicked')
+    }
+    checkBtn.addEventListener('click', handleClick)
 }
 
