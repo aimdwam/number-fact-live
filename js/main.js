b743485c70845d635bd8291a3e93477a802795be
Chris Cheung <chris.ch.cheung@gmail.com>
2020-03-19 08:40:59 +0800

    try fetching from numbersapi

--- js/main.js
+++ js/main.js
@@ -13,11 +13,30 @@ function onLoad() {
         return item
     }
 
+    let numberapi = num => {
+        let url = "http://numbersapi.com/" + num
+        let promise = fetch(url)
+        let resultPromise = promise.then(response => {
+            let textPromise = response.text()
+            console.log("textPromise: ", textPromise)
+            return textPromise
+        }, error => {
+            console.log(error)
+        })
+
+        resultPromise.then (text => {
+            console.log("text: ", text)
+        })
+        console.log('function finished')
+    }
+
     let handleClick = evt => {
         console.log('clicked')
         factList.append(createItem(input.value))
+
+        numberapi(input.value)
     }
-    
+
     checkBtn.addEventListener('click', handleClick)
 }
 
