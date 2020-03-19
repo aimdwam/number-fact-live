7d04fb976e99d4c261cf3e530b27a73074edb8b6
Chris Cheung <chris.ch.cheung@gmail.com>
2020-03-19 08:36:27 +0800

    append hardcoded element to factList onClick

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ console.log("main.js loaded")
 function onLoad() {
     console.log("body onload event fired")
 
+    let factList = document.getElementById("factList")
     let checkBtn = document.getElementById("checkBtn")
+
     let handleClick = evt => {
         console.log('clicked')
+        let item = document.createElement('li')
+        item.innerText = 'abc'
+        factList.append(item)
     }
     checkBtn.addEventListener('click', handleClick)
 }
